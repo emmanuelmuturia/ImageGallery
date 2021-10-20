9efbd2d4b9fe162a8f0d7289f31ce8706b3d707f
emmanuelmuturia <emmanuel.muturia@strathmore.edu>
2021-10-20 19:04:43 +0300

    Initial commit

---
+++ app/src/main/java/com/emmanuelmuturia/imagegallery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.emmanuelmuturia.imagegallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
